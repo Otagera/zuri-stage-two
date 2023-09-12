6b814d3664a29967dda88024d8a435523b303b9e
Othniel Agera <otagera@gmail.com>
2023-09-12 09:24:23 +0100

    Create and fetch

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
-const { Sequelize } = require("sequelize");
+const { Sequelize, DataTypes } = require("sequelize");
 
 const app = express();
 const router = express.Router({ mergeParams: true });
@@ -20,34 +20,47 @@ const day = [
 ];
 const sequelize = new Sequelize({
 	dialect: "sqlite",
-	storage: "/db.sqlite",
+	storage: "db.sqlite",
+});
+const User = sequelize.define("User", {
+	// Model attributes are defined here
+	firstName: {
+		type: DataTypes.STRING,
+		allowNull: false,
+	},
+	lastName: {
+		type: DataTypes.STRING,
+	},
 });
 (async () => {
 	try {
 		await sequelize.authenticate();
 		console.log("Connection has been established successfully.");
+		await sequelize.sync({ force: true });
+		console.log("All models were synchronized successfully.");
 	} catch (error) {
 		console.error("Unable to connect to the database:", error);
 	}
 })();
 
-router.get("/", async (req, res) => {
+router.post("/", async (req, res) => {
 	try {
-		const { slack_name, track } = req.query;
-		const isoDate = new Date().toISOString();
-		return res.status(200).send({
-			slack_name,
-			current_day: day[new Date().getUTCDay()],
-			utc_time:
-				isoDate.length === 24
-					? `${new Date().toISOString().slice(0, -5)}Z`
-					: `${new Date().toISOString()}`,
-			track,
-			github_file_url:
-				"https://github.com/Otagera/zuri-stage-one/blob/main/server.js",
-			github_repo_url: "https://github.com/Otagera/zuri-stage-one",
-			status_code: 200,
-		});
+		const { firstName, lastName } = req.body;
+		const user = await User.create({ firstName, lastName });
+		return res.status(200).send({ message: "User fetched successfully", user });
+	} catch (error) {
+		return res.status(500).send({ error: error.message || error });
+	}
+});
+
+router.get("/:user_id", async (req, res) => {
+	try {
+		const { user_id } = req.params;
+		const user = await User.findOne({ where: { id: user_id } });
+		if (!user) {
+			res.status(404).send({ message: "User not found" });
+		}
+		return res.status(200).send({ message: "User fetched successfully", user });
 	} catch (error) {
 		return res.status(500).send({ error: error.message || error });
 	}
